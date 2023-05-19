ca575dd33f11a499017c777524822e0c172e9390
socialtonics <mjkgarrow@gmail.com>
2023-05-19 12:25:25 +1000

    updated URL in front-end

--- client/src/utils/globals.js
+++ client/src/utils/globals.js
@@ -1,2 +1,2 @@
-export const URL = "https://mern-exercise-app-zy3k.onrender.com"
+export const URL = "https://mern-exercise-app-zy3k.onrender.com/api"
 // export const URL = "http://localhost:3001/api"
